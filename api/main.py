206e84ea7eba5373dce623093be8ce0cf989d17d
Artem <artem.kartashov@idea.edu.pl>
2023-12-09 23:17:58 +0100

    WIP

--- api/main.py
+++ api/main.py
@@ -1,12 +1,15 @@
 import uvicorn
 from fastapi import FastAPI
 
+from fastapi.staticfiles import StaticFiles
 from api.auth.base_config import auth_backend, fastapi_users
 from api.auth.schemas import UserRead, UserCreate
+from api.pages.router import router_pages
 from api.weigh_ins.router import weigh_router
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="api/static"), name="static")
 
 app.include_router(
     fastapi_users.get_auth_router(auth_backend),
@@ -19,6 +22,7 @@ app.include_router(
     tags=["auth"],
 )
 app.include_router(weigh_router)
+app.include_router(router_pages)
 
 # Keep it for debugging
 # if __name__ == "__main__":
--- api/weigh_ins/router.py
+++ api/weigh_ins/router.py
@@ -1,12 +1,18 @@
-from fastapi import APIRouter, Depends
+import datetime
+
+from fastapi import APIRouter, Depends, Request
 from sqlalchemy import insert, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.auth.base_config import current_user
 from api.auth.models import User
 from api.db.session import get_async_session
+from api.pages.router import templates
 from api.weigh_ins.models import Weigh
 from api.weigh_ins.schemas import WeighCreate
+from api.weigh_ins.service import get_history_weighs_plot
+
+import plotly.io as pio
 
 weigh_router = APIRouter(
     prefix="/weigh",
@@ -33,5 +39,23 @@ async def get_weigh_history(
 ):
     query = select(Weigh.weigh, Weigh.datetime).where(Weigh.user_id == user.id)
     result = await session.execute(query)
-    result_dict = {row._mapping.weigh: row._mapping.datetime.date() for row in result}
+    result_dict: dict[float | int, datetime.datetime] = {
+        # row._mapping.weigh: row._mapping.datetime.date() for row in result
+    }
     return result_dict
+
+
+@weigh_router.get("/plot")
+async def get_weigh_plot(
+    request: Request,
+    user: User = Depends(current_user),
+    session: AsyncSession = Depends(get_async_session),
+):
+    query = select(Weigh.weigh, Weigh.datetime).where(Weigh.user_id == user.id)
+    result = await session.execute(query)
+    result_list = [row for row in result.all()]
+    fig = await get_history_weighs_plot(result_list)
+    plotly_styles = pio.to_html(fig, full_html=False, include_plotlyjs=False)
+    plotly_script = pio.to_html(fig, full_html=False, include_plotlyjs=True)
+
+    return templates.TemplateResponse("plot.html", {"request": request, "plotly_styles": plotly_styles, "plotly_script": plotly_script})
